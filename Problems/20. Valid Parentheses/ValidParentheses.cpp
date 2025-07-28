a9f55a7a34423ef8942d01285d28ff18a86ee76f
DavidKN10 <davidrs23178@gmail.com>
2025-07-28 02:16:47 -0500

    added 20. Valid Parentheses

---
+++ Problems/20. Valid Parentheses/ValidParentheses.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+class Solution {
+public:
+	bool isValid(std::string s) {
+		std::stack<char> openBrackets;		
+		if (s.empty()) {
+			return false;
+		}
+		else if (s.length() == 1) {
+			return false;
+		}
+		else if (s[0] == ')' || s[0] == ']' || s[0] == '}') {
+			return false;
+		}
+		for (size_t i = 0; i < s.length(); i++) {
+			if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+				openBrackets.push(s[i]);
+				continue;
+			}
+			switch (s[i]) {
+			case ')':
+				if (openBrackets.empty() || openBrackets.top() != '(') {
+					return false;
+					break;
+				}
+				else {
+					openBrackets.pop();
+					break;
+				}
+			case '}':
+				if (openBrackets.empty() || openBrackets.top() != '{') {
+					return false;
+					break;
+				}
+				else {
+					openBrackets.pop();
+					break;
+				}
+			case ']':
+				if (openBrackets.empty() || openBrackets.top() != '[') {
+					return false;
+					break;
+				}
+				else {
+					openBrackets.pop();
+					break;
+				}	
+			}	
+		}
+		return openBrackets.empty();
+	}
+};
